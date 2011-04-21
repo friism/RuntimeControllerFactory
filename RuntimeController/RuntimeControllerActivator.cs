2f5ff1399281642984a3b4ca2a92d7cd1381c890
Michael Friis <friism@gmail.com>
2011-04-21 12:44:31 -0700

    implement IControllerActivator

--- RuntimeController/RuntimeControllerActivator.cs
+++ RuntimeController/RuntimeControllerActivator.cs
@@ -1,7 +1,14 @@
-﻿
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
 namespace Fusonic.Web.Mvc.RuntimeController
 {
-	public class RuntimeControllerActivator
+	public class RuntimeControllerActivator : IControllerActivator
 	{
+		public IController Create(RequestContext requestContext, Type controllerType)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
