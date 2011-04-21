bf1fbfb2721225e4c1b4a785323948c3f849e261
Michael Friis <friism@gmail.com>
2011-04-21 15:10:10 -0700

    implement IDependencyResolver

--- RuntimeController/RuntimeControllerDependencyResolver.cs
+++ RuntimeController/RuntimeControllerDependencyResolver.cs
@@ -1,7 +1,19 @@
-﻿
+﻿using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+
 namespace Fusonic.Web.Mvc.RuntimeController
 {
-	public class RuntimeControllerDependencyResolver
+	public class RuntimeControllerDependencyResolver : IDependencyResolver
 	{
+		public object GetService(Type serviceType)
+		{
+			return null;
+		}
+
+		public IEnumerable<object> GetServices(Type serviceType)
+		{
+			return new List<object>();
+		}
 	}
 }
